40e4fef069a16b7d8b4bb7b68f0ca61663dafac3
Kayra <KayraAlat@gmail.com>
2020-05-01 22:42:00 +0300

    Added initial app file

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const index = require('./server/routes/index')
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(index);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Application listening on port ${port}.`);
+});
