3ca1f158e50b2e8eeae49f988b1bd1c9e02fae05
Kayra <KayraAlat@gmail.com>
2020-05-05 22:16:00 +0300

    Added database client

---
+++ src/server/database.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+
+pool.on('connect', () => {
+    console.log('Database successfully connected.');
+});
+
+module.exports = {
+    query: (text, params) => pool.query(text, params)
+};
