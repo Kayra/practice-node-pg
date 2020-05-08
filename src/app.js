77aebed80e914fc3c6acae9e30453c8bc3c8206d
Kayra <KayraAlat@gmail.com>
2020-05-08 22:17:00 +0300

    Updated app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-const index = require('./server/routes/index')
 const express = require('express');
 const cors = require('cors');
+
+const indexRoute = require('./server/routes/index');
+const productRoutes = require('./server/routes/productRoutes'); 
 
 
 const app = express();
@@ -8,7 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use(index);
+app.use(indexRoute);
+app.use(productRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
