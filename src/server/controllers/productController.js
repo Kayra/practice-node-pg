eaee0788b747b05ebddb6214e7db95b715245fc2
Kayra <KayraAlat@gmail.com>
2020-05-11 22:03:00 +0300

    Added get by id controller

--- src/server/controllers/productController.js
+++ src/server/controllers/productController.js
@@ -19,10 +19,13 @@ exports.createProduct = async (req, res) => {
 
 };
 
-
 exports.listAllProducts = async (req, res) => {
-
     const response = await db.query("SELECT * FROM products ORDER BY product_name ASC");
     res.status(200).send(response.rows);
+};
 
-}
+exports.getProductById = async (req, res) => {
+    const productId = parseInt(req.params.id);
+    const response = await db.query("SELECT * FROM products WHERE product_id = $1", [productId]);
+    res.status(200).send(response.rows);
+};
