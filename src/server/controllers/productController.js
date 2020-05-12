75b7a8801aa43ef204ed2b4c945d80e76ebc9108
Kayra <KayraAlat@gmail.com>
2020-05-12 22:11:00 +0300

    Added put controller

--- src/server/controllers/productController.js
+++ src/server/controllers/productController.js
@@ -3,17 +3,17 @@ const db = require('../database');
 
 exports.createProduct = async (req, res) => {
 
-    const { productName, quantity, price } = req.body;
+    const { product_name, quantity, price } = req.body;
 
     await db.query(
         "INSERT INTO products (product_name, quantity, price) VALUES ($1, $2, $3)",
-        [productName, quantity, price ]
+        [product_name, quantity, price ]
     );
 
     res.status(201).send({
-        message: `Product ${productName} added successfully.`,
+        message: `Product ${product_name} added successfully.`,
         body: {
-            product: { productName, quantity, price }
+            product: { product_name, quantity, price }
         }
     });
 
@@ -29,3 +29,22 @@ exports.getProductById = async (req, res) => {
     const response = await db.query("SELECT * FROM products WHERE product_id = $1", [productId]);
     res.status(200).send(response.rows);
 };
+
+exports.updateProductById = async (req, res) => {
+
+    const productId = parseInt(req.params.id);
+    const { product_name, quantity, price } = req.body;
+
+    await db.query(
+        "UPDATE products SET product_name = $1, quantity = $2, price = $3 WHERE product_id = $4",
+        [product_name, quantity, price, productId]
+    );
+
+    res.status(200).send({
+        message: `Product ${product_name} added successfully.`,
+        body: {
+            product: { product_name, quantity, price }
+        }  
+    });
+
+}
