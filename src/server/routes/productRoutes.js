46d3cfd73ae3c1ade49511eaa7d7c8f62a89d2bf
Kayra <KayraAlat@gmail.com>
2020-05-14 22:32:00 +0300

    Updated product routes with delete controller

--- src/server/routes/productRoutes.js
+++ src/server/routes/productRoutes.js
@@ -6,5 +6,6 @@ router.post('/products', productController.createProduct);
 router.get('/products', productController.listAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.put('/products/:id', productController.updateProductById);
+router.delete('/products/:id', productController.deleteProductById);
 
 module.exports = router;
